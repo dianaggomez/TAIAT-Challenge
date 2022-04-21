d47b28075cb3c8253cb31e6e38b55e7730468b55
Diana Gomez <90724694+dianaggomez@users.noreply.github.com>
2022-04-20 18:44:16 -0700

    Add files via upload

--- marl_tintersection_hlc.py
+++ marl_tintersection_hlc.py
@@ -11,16 +11,40 @@ class HighLevelControllerEnv(gym.Env):
         self.observation_space = spaces.Box(low = -100, high = 100, shape= (12,3), dtype = np.float16) # needs to be size = (12,3) numpy array or maybe a tuple
 
     @property
-    def LowLevelControllerEnv(self, MultiAgentTIntersectionEnv):
-        pass
+    def LowLevelControllerEnv(self):
+        config = {
+            "horizon": 100000,
+            "vehicle_config": {
+                "vehicle_model": 's',
+                "lidar": {
+                    "num_lasers": 0, # the more dense, the more accurate but longer computation time
+                    "num_others": 4,
+                    "distance": 40
+                },
+                "show_lidar": False,
+            },
+            # "target_vehicle_configs": {
+            #     "vehicle_model": "default",  "spawn_lane_index": (FirstPGBlock.NODE_2, FirstPGBlock.NODE_3, 1), 
+            # },
+            #
+            "use_render": True,
+            "debug": False,
+            "allow_respawn": False,
+            "manual_control": True,
+            "num_agents": 12,
+            "delay_done": 0,
+            "num_RL_agents" : 0,
+        }
+        environment = MultiAgentTIntersectionEnv(config)
+        return environment
 
-    def check_AVs_done(self, d_agents):
+    def done(self, d_agents):
         left_array = np.array(self.LowLevelControllerEnv.left_queue)
         right_array = np.array(self.LowLevelControllerEnv.right_queue)
 
         vehicle_order = self.LowLevelControllerEnv.vehicle_order # [0,1,2,3,4,5,11,10,9,8,7,6]
-        all_vehcile_location = np.concatenate((list(left_array == 2), list(right_array == 2))) # [true, false ....]
-        AVs_index = list(compress(vehicle_order, all_vehcile_location)) # get AV index from all vehciles
+        all_vehicle_location = np.concatenate((list(left_array == 2), list(right_array == 2))) # [true, false ....]
+        AVs_index = list(compress(vehicle_order, all_vehicle_location)) # get AV index from all vehciles
 
         if (d_agents[AVs_index]==True).all():
             return True
@@ -31,7 +55,7 @@ class HighLevelControllerEnv(gym.Env):
         # give high level action to MultiAgentTIntersectionEnv
         o, r, d_agents, _ = self.LowLevelControllerEnv.step(action)
         # d will have done information for each agent
-        d = self.checkAVs_done(d_agents)
+        d = self.done(d_agents)
         # we do not necessarily need info, it may be an empty dict
         i = {}
 
@@ -52,3 +76,7 @@ class HighLevelControllerEnv(gym.Env):
 if __name__ == "__main__":
     env = HighLevelControllerEnv()
     print((env.observation_space).shape)
+    action = env.action_space.sample()
+    print(action)
+    env.step(action)
+
