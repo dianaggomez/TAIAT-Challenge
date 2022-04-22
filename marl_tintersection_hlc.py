be3317ed54ff1cfb00863687b07a41cfd5bcfee5
Diana Gomez <90724694+dianaggomez@users.noreply.github.com>
2022-04-22 14:45:20 -0700

    Add files via upload

--- marl_tintersection_hlc.py
+++ marl_tintersection_hlc.py
@@ -9,9 +9,9 @@ class HighLevelControllerEnv(gym.Env):
     def __init__(self):
         self.action_space = spaces.Tuple((spaces.Discrete(2), spaces.Discrete(2))) # Ask Peide: Does this have to be a numpy array?
         self.observation_space = spaces.Box(low = -100, high = 100, shape= (12,3), dtype = np.float16) # needs to be size = (12,3) numpy array or maybe a tuple
+        self.LowLevelControllerEnv = self.initialize_LowLevelControllerEnv()
 
-    @property
-    def LowLevelControllerEnv(self):
+    def initialize_LowLevelControllerEnv(self):
         config = {
             "horizon": 100000,
             "vehicle_config": {
@@ -33,24 +33,24 @@ class HighLevelControllerEnv(gym.Env):
             "manual_control": True,
             "num_agents": 12,
             "delay_done": 0,
-            "num_RL_agents" : 0,
+            "num_RL_agents" : 12 # no one has IDM
         }
         environment = MultiAgentTIntersectionEnv(config)
         environment.reset()
         vehicles = environment.vehicles
-        print("Vehicles!!!! ", environment.vehicles)
+        # print("Vehicles!!!! ", environment.vehicles)
         environment.generate_vehicle_queue(vehicles)
         return environment
 
     def done(self, d_agents):
-        left_array = np.array(self.LowLevelControllerEnv.left_queue)
-        right_array = np.array(self.LowLevelControllerEnv.right_queue)
+        left_array = np.array(self.LowLevelControllerEnv.orig_left_queue)
+        right_array = np.array(self.LowLevelControllerEnv.orig_right_queue)
 
         vehicle_order = self.LowLevelControllerEnv.vehicle_order # [0,1,2,3,4,5,11,10,9,8,7,6]
         all_vehicle_location = np.concatenate((list(left_array == 2), list(right_array == 2))) # [true, false ....]
         AVs_index = list(compress(vehicle_order, all_vehicle_location)) # get AV index from all vehciles
 
-        if (d_agents[AVs_index]==True).all():
+        if (np.array(list(d_agents.values()))[AVs_index]==True).all():
             return True
         else:
             return False
@@ -81,11 +81,12 @@ class HighLevelControllerEnv(gym.Env):
 
 if __name__ == "__main__":
     env = HighLevelControllerEnv()
-
+    print("Inital Queue: ", env.LowLevelControllerEnv.visualize_queue())
     # print((env.observation_space).shape)
     # action = env.action_space.sample()
     action = (0,1)
     print("Original High Level Action", action)
+
     env.step(action)
 
 
